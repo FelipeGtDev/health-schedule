799b56b64dcac327044a17ec7036d1af33d38977
felipe <audionoulb@gmail.com>
2024-04-16 09:49:51 -0300

    iniciando tela de agendamentos

--- src/app/page/calendar/calendar.component.ts
+++ src/app/page/calendar/calendar.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {MatButton} from "@angular/material/button";
+import {MatLabel} from "@angular/material/form-field";
 
 @Component({
   selector: 'app-calendar',
   standalone: true,
-  imports: [],
+    imports: [
+        MatButton,
+        MatLabel
+    ],
   templateUrl: './calendar.component.html',
   styleUrl: './calendar.component.scss'
 })
